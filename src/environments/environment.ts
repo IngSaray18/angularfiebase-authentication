3dee037b907a44c4dfc42e481f33adc7cf53d1eb
Diego NB <diegonavab7@gmail.com>
2022-09-21 13:58:48 -0500

    red

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,22 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+    firebase: {
+    apiKey: "AIzaSyBs9fZySzcJPO1wcZ49kHEKLt6jWxBbeWI",
+    authDomain: "auth-cd57f.firebaseapp.com",
+    databaseURL: "https://auth-cd57f-default-rtdb.firebaseio.com",
+    projectId: "auth-cd57f",
+    storageBucket: "auth-cd57f.appspot.com",
+    messagingSenderId: "131397799756",
+    appId: "1:131397799756:web:62941a24b49eb7adff49e9",
+     measurementId: "G-DZTZ6393XY"
+  }
+
 };
+
+
 
 /*
  * For easier debugging in development mode, you can import the following file
